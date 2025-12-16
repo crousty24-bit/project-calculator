3e1492d28f58a1752ebe341e755699f3642f48a6
crousty24-bit <242641564+crousty24-bit@users.noreply.github.com>
2025-12-16 19:11:06 +0100

    add 4 basic operators func

---
+++ calculator.js
@@ -0,0 +1,12 @@
+// 1. operators functions :
+const add = (a, b) => a + b ;
+console.log(add(2, 4))
+
+const subtract = (a, b) => a - b;
+console.log(subtract(4, 2))
+
+const multiply = (a, b) => a * b;
+console.log(multiply(2, 4))
+
+const divide = (a, b) => a / b;
+console.log(divide(4, 2))
